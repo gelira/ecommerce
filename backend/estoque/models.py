975e57bae733b16a92f619d0a9a1d705d6d2e9bb
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-01 15:28:25 -0300

    models estoque

--- backend/estoque/models.py
+++ backend/estoque/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Loja(models.Model):
+    dono = models.ForeignKey(
+        to='acesso.Usuario',
+        on_delete=models.PROTECT,
+        related_name='lojas'
+    )
+    nome = models.CharField(
+        max_length=100
+    )
+    nome_url = models.CharField(
+        max_length=100
+    )
+
+class Produto(models.Model):
+    loja = models.ForeignKey(
+        to=Loja,
+        on_delete=models.PROTECT,
+        related_name='produtos'
+    )
+    nome = models.CharField(
+        max_length=100
+    )
+    descricao = models.TextField(
+        blank=True
+    )
+    preco = models.FloatField()
+
+class Foto(models.Model):
+    produto = models.OneToOneField(
+        to=Produto,
+        on_delete=models.PROTECT
+    )
+    region = models.CharField(
+        max_length=100
+    )
+    bucket = models.CharField(
+        max_length=100
+    )
+    key = models.CharField(
+        max_length=100
+    )
+
+class Versao(models.Model):
+    foto = models.ForeignKey(
+        to=Foto,
+        on_delete=models.PROTECT,
+        related_name='versoes'
+    )
+    version_id = models.CharField(
+        max_length=100
+    )
+    atual = models.BooleanField(
+        default=False
+    )
