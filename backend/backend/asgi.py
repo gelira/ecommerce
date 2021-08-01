2541d8a0a630478b2d97c845f49e06592d53b3f2
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-01 12:27:34 -0300

    carregando arquivo .env

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import dotenv
+from pathlib import Path
 from django.core.asgi import get_asgi_application
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv.read_dotenv(BASE_DIR / '.env')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import dotenv
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv.read_dotenv(BASE_DIR / '.env')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
