4cee5789f63b7700f310d8e5d4677ec2a0710f4a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-01 14:47:57 -0300

    cadastro dos clientes

--- backend/acesso/serializers.py
+++ backend/acesso/serializers.py
@@ -1,8 +1,42 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework_simplejwt.tokens import AccessToken
+
+from acesso.models import Usuario
 
 class AccesTokenSerializer(TokenObtainSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         return {
             'token': data['access']
+        }
+
+class CreateClienteSerializer(serializers.Serializer):
+    nome = serializers.CharField(
+        min_length=2,
+        max_length=150
+    )
+    email = serializers.EmailField(
+        max_length=150
+    )
+    senha = serializers.CharField(
+        min_length=6
+    )
+
+    def validate_email(self, value):
+        if Usuario.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Email já utilizado')
+        return value
+
+    def save(self):
+        nome = self.validated_data['nome']
+        email = self.validated_data['email']
+        senha = self.validated_data['senha']
+
+        u = Usuario.objects.create_user(email, email=email, password=senha, first_name=nome)
+
+        token = AccessToken.for_user(u)
+
+        return {
+            'token': str(token)
         }
--- backend/acesso/views.py
+++ backend/acesso/views.py
@@ -1,5 +1,19 @@
 from rest_framework_simplejwt.views import TokenViewBase
-from acesso.serializers import AccesTokenSerializer
+from rest_framework.views import (
+    APIView, 
+    Response
+)
+from acesso.serializers import (
+    AccesTokenSerializer,
+    CreateClienteSerializer
+)
 
 class AccessTokenView(TokenViewBase):
     serializer_class = AccesTokenSerializer
+
+class CreateClienteView(APIView):
+    def post(self, request):
+        ser = CreateClienteSerializer(data=request.data)
+        ser.is_valid(raise_exception=True)
+        data = ser.save()
+        return Response(data)
