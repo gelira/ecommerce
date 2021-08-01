6a3778701ebfadcccf9fd2c7a5b875f1b56f5bbb
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-01 14:53:27 -0300

    correção no login

--- backend/acesso/serializers.py
+++ backend/acesso/serializers.py
@@ -5,10 +5,14 @@ from rest_framework_simplejwt.tokens import AccessToken
 from acesso.models import Usuario
 
 class AccesTokenSerializer(TokenObtainSerializer):
+    def get_token(self):
+        token = AccessToken.for_user(self.user)
+        return str(token)
+
     def validate(self, attrs):
-        data = super().validate(attrs)
+        super().validate(attrs)
         return {
-            'token': data['access']
+            'token': self.get_token()
         }
 
 class CreateClienteSerializer(serializers.Serializer):
