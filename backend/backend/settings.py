a5fc2a151bd8325c2d8a8104741c8433375c40ac
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-04 00:20:18 -0300

    verificando id da loja em middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'shared.middlewares.LojaIdMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
