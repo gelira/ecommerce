e11dd4c2f79257d7ba2098354797e1ccc3d22f12
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-05 13:28:47 -0300

    rotas do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,16 @@ import {
   AccountCircle
 } from '@material-ui/icons';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  Switch,
+  Route,
+  useHistory
+} from "react-router-dom";
+
 import { fetchLojaAsync } from './store/acesso';
 
+import Login from './components/Login';
+import Registro from './components/Registro';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -27,15 +35,31 @@ const useStyles = makeStyles(() => ({
 function App() {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const nome_loja = useSelector(state => state.acesso.nome_loja);
   const usuario_id = useSelector(state => state.acesso.id);
+  const role = useSelector(state => state.acesso.role);
 
   useEffect(() => {
     const { host } = window.location;
     const nome_url = host.split('.')[0];
     dispatch(fetchLojaAsync({ nome_url }));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (usuario_id && role) {
+      if (role === 'dono') {
+        history.push('/compras');
+      }
+      else {
+        history.push('/produtos');
+      }
+    }
+    else {
+      history.push('/login');
+    }
+  }, [usuario_id, role, history]);
 
   return (
     <>
@@ -59,7 +83,20 @@ function App() {
           </Toolbar>
         </AppBar>
         <Container>
-          <h1>Teste</h1>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/registro">
+              <Registro />
+            </Route>
+            <Route path="/produtos">
+              <h1>Produtos</h1>
+            </Route>
+            <Route path="/compras">
+              <h1>Compras</h1>
+            </Route>
+          </Switch>
         </Container>
       </div>
     </>
