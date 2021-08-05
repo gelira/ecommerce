cb9949332518877255de5208edaf6e5ccf266b40
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-05 17:02:07 -0300

    Formulário de produtos

---
+++ frontend/src/components/ProdutoForm.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { 
+  Button, 
+  Dialog, 
+  DialogActions, 
+  DialogContent, 
+  DialogTitle, 
+  TextField 
+} from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { 
+  closeProdutoForm, 
+  produtoCleanState,
+  sendProdutoAsync 
+} from '../store/estoque';
+
+export default function ProdutoForm() {
+  const dispatch = useDispatch();
+  
+  const open = useSelector(state => state.estoque.produtoForm);
+  const produto = useSelector(state => {
+    const { id, produtos } = state.estoque;
+    return produtos.find(p => p.id === id);
+  });
+
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [preco, setPreco] = useState(0);
+  const [foto, setFoto] = useState('');
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    if (produto) {
+      setNome(produto.nome);
+      setDescricao(produto.descricao);
+      setPreco(produto.preco);
+      setFoto(produto.foto);
+    }
+  }, [produto]);
+
+  const close = () => {
+    dispatch(produtoCleanState());
+    dispatch(closeProdutoForm());
+  };
+
+  const salvar = async () => {
+    await dispatch(sendProdutoAsync({ nome, descricao, preco, file }));
+    close();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Formulário de Produto</DialogTitle>
+      <DialogContent>
+        <TextField
+          margin="dense"
+          label="Nome do produto"
+          value={nome}
+          onChange={e => setNome(e.target.value)}
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          label="Descrição"
+          value={descricao}
+          onChange={e => setDescricao(e.target.value)}
+          fullWidth
+          multiline
+        />
+        <TextField
+          margin="dense"
+          label="Nome do produto"
+          type="number"
+          min="0"
+          step="0.01"
+          value={preco}
+          onChange={e => setPreco(Number(e.target.value))}
+          fullWidth
+        />
+        {foto && (
+          <img alt="" src={foto} height="100" />
+        )}
+        <TextField
+          margin="dense"
+          label="Foto do produto"
+          type="file"
+          accept="image/*"
+          onChange={e => setFile(e.target.files[0])}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={close}>
+          Cancelar
+        </Button>
+        <Button onClick={salvar} color="primary">
+          Salvar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
