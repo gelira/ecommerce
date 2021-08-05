6c085191bad6ad006c5148c16c195a937031e985
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-08-05 01:24:42 -0300

    mudando modo de importação App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import { 
+  CssBaseline,
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+import {
+  AccountCircle
+} from '@material-ui/icons';
+
 
 const useStyles = makeStyles(() => ({
   root: {
